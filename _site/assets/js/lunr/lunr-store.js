c2dcdc9e11e9c9c5c4c8c03a8daa4bfdf89ab586
cmpark0126 <cmpark@cmpark-MacBook-Air.local>
2018-08-06 17:52:05 +0900

    Update Theme and bio

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,9 +1,4 @@
 var store = [{
-        "title": "Jekyll 블로그 테마 적용하기 (minimal-mistakes)",
-        "excerpt":"jekyll 블로그를 시작하기 위한 준비를 저번 포스트에서 끝냈다.이제 테마를 적용하고자 여러 테마를 찾던중 정말 괜찮은 테마를 발견했고 그것을 적용하는 과정을 포스팅하려고 한다.테마의 이름은 ‘Minimal Mistakes’로 github star와 fork를 보아 인기있는 테마인듯 싶다.심플한 디자인과 가독성이 맘에 들었고 무엇보다 문서가 너무나 잘 정리되있다는 점에 있어서 선택하게 되었다.Minimal Mistakes Official &amp; Demo Page...","categories": [],
-        "tags": ["blog","jekyll"],
-        "url": "http://localhost:4000/jekyll-apply-theme/",
-        "teaser":null},{
         "title": "cuDNN - Host memory에서 Device memory로 옮기는 법",
         "excerpt":"cuDNN_Host2Memory Step 1: Remove Theme Files Step 2: Update Gemfile Step 3: Run Bundler Step 4: Install the Theme Step 5: jekyll new Tweaks cuDNN은 nvidia gpu를 사용할 때, 특히 딥러닝 파트를 구현할 때, 거의 필수적으로 사용되고 있는 라이브러리이다.물론 일부 라이브러리는 cuda를 이용해서 딥러닝 알고리즘을 직접 구현하기도 하지만 (대부분의 슈퍼...","categories": ["Develop"],
         "tags": ["cuDNN","WICWIU","DeepLearning","Framework"],
