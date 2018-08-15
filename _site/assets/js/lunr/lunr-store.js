b56d854b46ba85290aacd06effcaa7412c88549b
cmpark0126 <cmpark@cmpark-MacBook-Air.local>
2018-08-16 00:53:22 +0900

    Modify Post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "cuDNN(0) - WICWIU LINUX GPU 버전 설치 : 필요없는 분들은 넘어가셔도 문제가 없습니다.",
-        "excerpt":"이 글은 우리 연구실에서 개발한 WICWIU API를 이해시키기 위한 용도로 사용될 것이기 때문에,일반적인 설명과는 조금 다를 것이라는 것에서 양해를 구한다.설명에서는 WICWIU의 설명을 겸하여 진행한다. cuDNN_Host2Memory WICWIU란? 왜 cuDNN을? cuDNN이란? cuDNN 라이브러리 사용준비하기 (WICWIU를 중심으로) 끝 WICWIU란?WICWIU는 국내 대학 최초로 공개하는 딥러닝 오픈소스 프레임워크로,나는 이 프레임워크을 설계하고 개발하는 팀의 1기로 참여하여...","categories": ["Develop"],
-        "tags": ["cuDNN","WICWIU","DeepLearning","Framework"],
-        "url": "http://localhost:4000/develop/cuDNN-0-InstallWICWIU/",
+        "title": "WICWIU(0) - WICWIU LINUX GPU 버전 설치",
+        "excerpt":"참고 : 이 글은 한동대학교 김인중 교수님 연구실에서 개발한 WICWIU API를 이해시키기 위한 목적으로 쓰여진 글입니다.WICWIU란?WICWIU는 국내 대학 최초로 공개하는 딥러닝 오픈소스 프레임워크로,나(박천명)는 이 프레임워크을 설계하고 개발하는 팀의 1기로 참여하여 진행했다.현재는 3기까지 프로젝트가 진행되었으며, 프로그램도 어느정도 규모를 가지게 되었다.그렇기에 이 블로그 글은 개인적인 정보 정리와 함께 인수인계 자료를 겸하고자 한다.cuDNN...","categories": [],
+        "tags": ["WICWIU"],
+        "url": "http://localhost:4000/WICWIU_0/",
         "teaser":null}]
